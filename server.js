6c6ccabd7f47d2a0ec7329134bd09bceddcd39ac
RadekKosiada <kosiadar@gmail.com>
2019-07-03 20:25:45 +0200

    Get data from geo api

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+const secrets = require("./secrets.json");
 
 const app = express();
 const port = 5000;
 
 const someData = async () => {
   try {
-    return await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-  } catch(err) {
+    return await axios.get(
+      `https://api.opencagedata.com/geocode/v1/json?q=Moabit%2C%20Berlin&key=${
+        secrets.API_KEY
+      }&language=en&pretty=1`
+    );
+  } catch (err) {
     console.log(err.message);
   }
-}
-let title = '';
+};
+let type = "";
+let lat = "";
+let lng = "";
 const checkData = async () => {
-  const response = await someData()
+  const response = await someData();
 
   if (response.data) {
-    console.log(response.data.title);
-    title = response.data.title;
+    type = response.data.results[0].components._type;
+    lat = response.data.results[0].geometry.lat;
+    lng = response.data.results[0].geometry.lng;
+    console.log(type, lat, lng);
   }
-}
+};
 
 checkData();
 
-app.get('/api', (req, res) => { 
-  const string = "Hello World! " + title;
+app.get("/api", (req, res) => {
+  const string = "Hello World! ";
   res.json(string);
 });
 
