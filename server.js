3e88b375172b31f4d269506f75629224a59b1f68
RadekKosiada <kosiadar@gmail.com>
2019-07-03 20:36:40 +0200

    Rename function to fetch data; Adjust url

--- server.js
+++ server.js
@@ -5,22 +5,27 @@ const secrets = require("./secrets.json");
 const app = express();
 const port = 5000;
 
-const someData = async () => {
-  try {
-    return await axios.get(
-      `https://api.opencagedata.com/geocode/v1/json?q=Moabit%2C%20Berlin&key=${
-        secrets.API_KEY
-      }&language=en&pretty=1`
-    );
-  } catch (err) {
-    console.log(err.message);
-  }
-};
 let type = "";
 let lat = "";
 let lng = "";
-const checkData = async () => {
-  const response = await someData();
+let searchQuery = "Victory Column"
+let location = "";
+
+const getData = async () => {
+  try {
+    return await axios.get(
+      `https://api.opencagedata.com/geocode/v1/json?q=${searchQuery}%2C%20${location}&key=${
+        secrets.API_KEY
+      }&language=en&pretty=1&countrycode=de`
+    );
+  } catch (err) {
+    console.log("Error in getData(): ", err.message);
+  }
+};
+
+const handleData = async () => {
+  try {
+    const response = await getData();
 
   if (response.data) {
     type = response.data.results[0].components._type;
@@ -28,12 +33,16 @@ const checkData = async () => {
     lng = response.data.results[0].geometry.lng;
     console.log(type, lat, lng);
   }
+  } catch(err) {
+    console.log("Error in handleData(): ", err.message);
+  }
+  
 };
 
-checkData();
+handleData();
 
 app.get("/api", (req, res) => {
-  const string = "Hello World! ";
+  const string = "Hello World! " + type;
   res.json(string);
 });
 
