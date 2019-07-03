f0d0e102125562385d2eb34326f521eab3566ec2
RadekKosiada <kosiadar@gmail.com>
2019-07-03 19:59:51 +0200

    Set axios on server to fetch API; Add secrets.json

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
-
-app.get('/api', (req, res) => {
-  const string = "Hello World!"
-  res.json(string);
-})
-
 const port = 5000;
+
+const someData = async () => {
+  try {
+    return await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+  } catch(err) {
+    console.log(err.message);
+  }
+}
+let title = '';
+const checkData = async () => {
+  const response = await someData()
+
+  if (response.data) {
+    console.log(response.data.title);
+    title = response.data.title;
+  }
+}
+
+checkData();
+
+app.get('/api', (req, res) => { 
+  const string = "Hello World! " + title;
+  res.json(string);
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
